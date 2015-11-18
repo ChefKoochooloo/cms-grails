40eed6bc555f2b54a3149431472902323148f12c
ikakara <allen.arakaki@gmail.com>
2015-11-18 11:55:08 -0800

    config less

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -67,23 +67,17 @@ grails.project.dependency.resolution = {
 
     compile ":scaffolding:2.1.2"
     compile ':cache:1.1.8'
-    // asset-pipeline 2.0+ requires Java 7, use version 1.9.x with Java 6
     compile ":asset-pipeline:2.5.7"
+    compile ":less-asset-pipeline:2.5.4"
+
+    // https://github.com/groovydev/twitter-bootstrap-grails-plugin/blob/master/README.md
+    //compile ':twitter-bootstrap:3.3.5' // 3.3.0 is broken https://github.com/groovydev/twitter-bootstrap-grails-plugin/issues/121
 
     // plugins needed at runtime but not for compilation
+    //runtime ":jquery:1.11.1"
+    
     runtime ":hibernate4:4.3.10" // or ":hibernate:3.6.10.18"
     //runtime ":database-migration:1.4.0"
-    runtime ":jquery:1.11.1"
-
-    // Uncomment these to enable additional asset-pipeline capabilities
-    //compile ":sass-asset-pipeline:1.9.0"
-    compile ":less-asset-pipeline:2.5.4"
-    //compile ":coffee-asset-pipeline:1.8.0"
-    //compile ":handlebars-asset-pipeline:1.3.0.3"
-
-    //
-    runtime ':twitter-bootstrap:3.3.5'
-    runtime ':angularjs-resources:1.4.2'
 
   }
 }
